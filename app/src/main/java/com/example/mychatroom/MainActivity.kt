879529f5633df97503ae087d47abcffc65b96d3e
Namlhd <namlhd@sdtech.vn>
2025-06-26 20:46:15 +0700

    upload

--- app/src/main/java/com/example/mychatroom/MainActivity.kt
+++ app/src/main/java/com/example/mychatroom/MainActivity.kt
@@ -6,19 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.rememberNavController
-import com.example.mychatroom.session.SessionManager
-import com.example.mychatroom.session.SettingManager
-import com.example.mychatroom.viewModel.AuthViewModel
 import com.example.mychatroom.screen.MainScreen
 
 class MainActivity : ComponentActivity() {
